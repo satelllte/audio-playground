4bdcdba6f8d0bbfb82858397be67b5d95c0a86c6
Yehor M <20585619+satelllte@users.noreply.github.com>
2024-02-17 18:09:14 +0200

    make heading selectable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 export default function () {
   return (
     <div>
-      <h1 className='select-none p-4 text-2xl font-bold sm:p-8 sm:text-4xl'>
-        Tone
-      </h1>
-      <div className='flex flex-col gap-2 px-4 sm:px-8'>
-        Hello!
-      </div>
+      <h1 className='p-4 text-2xl font-bold sm:p-8 sm:text-4xl'>Tone</h1>
+      <div className='flex flex-col gap-2 px-4 sm:px-8'>Hello!</div>
     </div>
   );
 }
