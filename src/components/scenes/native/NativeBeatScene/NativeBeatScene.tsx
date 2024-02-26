884aacd9d06a634ac7bbcf6d0db7e8617abe05a2
Yehor M <20585619+satelllte@users.noreply.github.com>
2024-02-26 19:03:37 +0200

    basic beat

--- src/components/scenes/native/NativeBeatScene/NativeBeatScene.tsx
+++ src/components/scenes/native/NativeBeatScene/NativeBeatScene.tsx
@@ -1,4 +1,5 @@
 'use client';
+import {type Unpromisify} from '@/utils/types';
 import {NativeScene} from '../NativeScene';
 
 export function NativeBeatScene() {
@@ -6,27 +7,115 @@ export function NativeBeatScene() {
 }
 
 const renderAudio = async (sampleRate: number): Promise<AudioBuffer> => {
-  const duration = 1;
-  const offlineContext = new OfflineAudioContext(
-    2,
-    sampleRate * duration,
-    sampleRate,
-  );
-  const now = offlineContext.currentTime;
+  const tempo = 145;
+  const barsCount = 2;
 
-  const oscillator = new OscillatorNode(offlineContext, {
-    frequency: 110,
-    type: 'sine',
+  const beatDuration = 60 / tempo;
+  const barDuration = beatDuration * 4; // 4/4 time signature
+  const duration = barsCount * barDuration;
+
+  const context = new OfflineAudioContext(2, sampleRate * duration, sampleRate);
+  const samples = await fetchSamples(context);
+
+  const now = context.currentTime;
+
+  for (let barIndex = 0; barIndex < barsCount; barIndex++) {
+    loop({
+      context,
+      startAt: now + barDuration * barIndex,
+      barDuration,
+      samples,
+    });
+  }
+
+  return context.startRendering();
+};
+
+const loop = ({
+  context,
+  startAt,
+  barDuration,
+  samples,
+}: {
+  context: BaseAudioContext;
+  startAt: number;
+  barDuration: number;
+  samples: Unpromisify<ReturnType<typeof fetchSamples>>;
+}): void => {
+  const durationEighth = barDuration / 8;
+
+  // Kick
+  [0, 1].forEach((i) => {
+    playSample({
+      context,
+      buffer: samples.kick,
+      startAt: startAt + durationEighth * i,
+      duration: samples.kick.duration,
+    });
   });
 
-  oscillator.connect(offlineContext.destination);
+  // Snare
+  [4].forEach((i) => {
+    playSample({
+      context,
+      buffer: samples.snare,
+      startAt: startAt + durationEighth * i,
+      duration: samples.snare.duration,
+    });
+  });
 
-  oscillator.start(now);
-  oscillator.stop(now + duration);
+  // Hi-hat
+  [0, 1, 2, 3, 4, 4.5, 5, 6, 7].forEach((i) => {
+    playSample({
+      context,
+      buffer: samples.hiHat,
+      startAt: startAt + durationEighth * i,
+      duration: samples.hiHat.duration,
+    });
+  });
+};
 
-  oscillator.onended = () => {
-    oscillator.disconnect(offlineContext.destination);
+const playSample = ({
+  context,
+  buffer,
+  startAt,
+  duration,
+}: {
+  context: BaseAudioContext;
+  buffer: AudioBuffer;
+  startAt: number;
+  duration: number;
+}): AudioBufferSourceNode => {
+  const source = new AudioBufferSourceNode(context, {buffer});
+  source.connect(context.destination);
+  source.start(startAt, 0, duration);
+  source.onended = () => {
+    source.disconnect();
   };
 
-  return offlineContext.startRendering();
+  return source;
+};
+
+const fetchSamples = async (context: BaseAudioContext) => {
+  const basePath = '/static/samples';
+  const [hiHat, kick, snare] = await Promise.all([
+    fetchAudioFile({path: `${basePath}/hi_hat_1.wav`, context}),
+    fetchAudioFile({path: `${basePath}/kick_1.wav`, context}),
+    fetchAudioFile({path: `${basePath}/snare_1.wav`, context}),
+  ]);
+  return {hiHat, kick, snare};
+};
+
+const fetchAudioFile = async ({
+  path,
+  context,
+}: {
+  path: string;
+  context: BaseAudioContext;
+}): Promise<AudioBuffer> => {
+  const res = await fetch(path);
+  if (!res.ok) throw new Error(`Failed to fetch audio file from: "${path}"`);
+
+  const arrayBuffer = await res.arrayBuffer();
+  return context.decodeAudioData(arrayBuffer);
 };
